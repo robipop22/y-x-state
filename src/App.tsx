efd2d56b63cc4f74fc3d9e9db35ce11d1a085e0c
robipop22 <poprobertdaniel22@gmail.com>
2022-05-09 13:19:02 +0300

    add a hi screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import BooksScreen from './screens/Books/index';
 import TrafficLightsScreen from './screens/TrafficLight';
 
-const App = () => <BooksScreen />;
+// const App = () => <BooksScreen />;
 // const App = () => <TrafficLightsScreen />;
+const App = () => <h1 className="font-bold text-3xl text-center">Hi 👋</h1>;
 export default App;
